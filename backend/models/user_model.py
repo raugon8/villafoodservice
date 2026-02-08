c4b722facfedaece4156a411a5bbb45148a25d95
RauGon8 <raulgonzalezherrera8@gmail.com>
2026-02-08 19:59:49 +0000

    actualizacion de user_model
    Esto es mas bien para que concuerde bien con lo añadido del codigo.

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, Integer, String
-# Importamos la configuración del archivo anterior
-from backend.database_manager.database import Base
+from sqlalchemy.orm import relationship
+from database_manager.database import Base
 
-class User(Base):
-    # Nombre de la tabla en la base de datos
+class UserModel(Base):
     __tablename__ = "usuarios"
 
-    # Columnas exactas que pide el PDF
-    usuario_ID = Column(Integer, primary_key=True, autoincrement=True)
+    # Columns using snake_case
+    usuario_id = Column(Integer, primary_key=True, autoincrement=True)
     nombre_usuario = Column(String, nullable=False)
     correo = Column(String, unique=True, nullable=False)
     contraseña = Column(String, nullable=False)
+
+    # Relation with orders table
+    orders = relationship("OrderModel", back_populates="user")
